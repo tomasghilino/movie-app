3bdc9ed89456f4687ee57f8af5bed4abf239e739
Franco Brumatti <franco.brumatti@gmail.com>
2022-04-28 14:57:09 -0300

    Footer prototype created

--- components/Layout/Footer/index.jsx
+++ components/Layout/Footer/index.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
+//styles
+import { FooterLayout, FooterContent, FooterBottomText, FooterIcons, FooterButtons } from './FooterElements';
+
+//icons
+import { ImFacebook, ImInstagram, ImTwitter, ImYoutube } from 'react-icons/im';
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <FooterLayout>
+
+      <FooterContent>
+
+        <FooterIcons>
+          <ImFacebook />
+          <ImInstagram />
+          <ImTwitter />
+          <ImYoutube />
+        </FooterIcons>
+
+        <FooterButtons>
+          <a href="#">Términos de uso</a>
+          <a href="#">Política de Privacidad</a>
+          <a href="#">Gestión de cookies</a>
+        </FooterButtons>
+
+      </FooterContent>
+
+      <FooterBottomText>&copy; 1998 - 2023 ElPibeServer - Todos los derechos reservados</FooterBottomText>
+    </FooterLayout>
+  );
 };
 
 export default Footer;
