8340550e7335ad95a37b605910894a8a12cc11d9
Tomas Ghilino <tomasghilino01@gmail.com>
2022-04-28 21:17:20 -0300

    random movie fetch hero

--- hooks/useHeroMovie.jsx
+++ hooks/useHeroMovie.jsx
@@ -12,15 +12,21 @@ const useHeroMovie = () => {
     const apikey = process.env.NEXT_PUBLIC_API_KEY_MOVIE;
     try {
       const res = await axios.get(
-        `https://api.themoviedb.org/3/tv/popular?api_key=${apikey}&page=1`
+        `https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&page=1`
+      );
+
+      console.log(res);
+
+      const randomPopularMovie = Math.floor(
+        Math.random() * res.data.results.length
       );
 
       setHeroData({
         ...heroData,
-        src: `https://image.tmdb.org/t/p/original${res.data.results[0].backdrop_path}`,
-        name: res.data.results[0].name,
-        vote: res.data.results[0].vote_average,
-        id: res.data.results[0].id,
+        src: `https://image.tmdb.org/t/p/original${res.data.results[randomPopularMovie].backdrop_path}`,
+        name: res.data.results[randomPopularMovie].title,
+        vote: res.data.results[randomPopularMovie].vote_average,
+        id: res.data.results[randomPopularMovie].id,
       });
     } catch (error) {
       console.log(error);
